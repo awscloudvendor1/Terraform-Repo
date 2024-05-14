7b9203c00bfc4eabd7a98fbc2c1406104914cf64
awscloudvendor1 <83281099+awscloudvendor1@users.noreply.github.com>
2024-05-14 10:51:10 +0530

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -9,10 +9,10 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ca-central-1"
+  region = "ap-south-1"
 }
 
 # Create a VPC
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "192.168.1.0/26"
 }
